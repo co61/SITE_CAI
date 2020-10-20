6dfcf96ff40ff2e743cc38a324408ef17499331e
Corentin Régné <corentin.regne@gmail.com>
2020-10-20 15:00:13 +0200

    update page, adding js for form et search bar

--- JS/rayon.js
+++ JS/rayon.js
@@ -1,27 +1,26 @@
-        // Mon code Javascript
-        let rayonpict1 = document.getElementById("rayonpict1");
-        let rayonpict2 = document.getElementById("rayonpict2");
-        let btnRayon = document.getElementById("btnRayon");
+let rayonpict1 = document.getElementById("rayonpict1");
+let rayonpict2 = document.getElementById("rayonpict2");
+let btnRayon = document.getElementById("btnRayon");
 
-        btnRayon.addEventListener("click", () => 
-        {
+btnRayon.addEventListener("click", () => 
+{
 
-            if (document.getElementById("rechercheRayon").value == "tronçonneuse") 
-            {
-                rayonpict1.src ="../img/rayon1_tronconneuse.png";
-                rayonpict2.src ="../img/rayon2.png";
-            }
+    if (document.getElementById("rechercheRayon").value == "tronçonneuse") 
+    {
+        rayonpict1.src ="../img/rayon1_tronconneuse.png";
+        rayonpict2.src ="../img/rayon2.png";
+    }
 
-            else if (document.getElementById("rechercheRayon").value == "tondeuse") 
-            {
-                rayonpict1.src ="../img/rayon1_tondeuse.png";
-                rayonpict2.src ="../img/rayon2.png";
-            }
+    else if (document.getElementById("rechercheRayon").value == "tondeuse") 
+    {
+        rayonpict1.src ="../img/rayon1_tondeuse.png";
+        rayonpict2.src ="../img/rayon2.png";
+    }
 
-            else if (document.getElementById("rechercheRayon").value == "sécateur") 
-            {
-                rayonpict2.src ="../img/rayon2_secateur.png";
-                rayonpict1.src ="../img/rayon1.png";
-            }
-    
-        })
+    else if (document.getElementById("rechercheRayon").value == "sécateur") 
+    {
+        rayonpict2.src ="../img/rayon2_secateur.png";
+        rayonpict1.src ="../img/rayon1.png";
+    }
+
+})
